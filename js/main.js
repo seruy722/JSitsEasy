099ab0f82181a56e43e0dc3610e4192c80357851
sergey <seruy722@gmail.com>
2017-02-24 20:09:34 +0200

    Function sum

---
+++ js/main.js
@@ -0,0 +1,17 @@
+
+"use strict";
+/*
+1. Напишите функцию, которая принимает 2 аргумента и отображает их сумму в консоле
+*/
+
+// function sum(num1, num2) {
+//   return num1 + num2;
+// }
+
+let sum = ((num1, num2) => num1 + num2);
+
+
+console.log(sum(3, 5)); //8
+console.log(sum(10, 50)); // 60
+
+

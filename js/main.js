f26195fe09062f2661276676848325bb5aca475c
sergey <seruy722@gmail.com>
2017-02-24 23:56:41 +0200

    Functions (FizzBuzz)

--- js/main.js
+++ js/main.js
@@ -1,81 +1,124 @@
 ﻿
 'use strict';
-let favoriteCars = ['BMW', 'Reno', 'Subaru'];
-
-let user = {
-  name: 'Sergey',
-  surname: 'Denisiuk',
-  dateOfBith: 1989,
-  favoriteCars,
-  friends: [{ name: 'Vasja', surname: 'Petrovych' }, { name: 'Kolja', surname: 'Shevko' }]
-};
-
-console.log(user.friends);
-console.log(user.friends[0]);
-console.log(user.favoriteCars);
-
 /*
- 1. Составить предложение из вышестоящих переменных:
- "Сколько нужно программистов чтобы сделать проект ? 25, 1, команда"
- */
-
-let numbers = [25, 1];
-let project = "проект";
-let team = `команда`;
-let howMuch = "Сколько";
-let sentence = 'нужно программистов чтобы сделать проект ?';
-
-let task1 = `${howMuch} ${sentence} ${numbers} ${team}`;
-console.log(task1);
-
-
-/*
- 2. Составьте предложение из представленного массива и выведите предложение
- в консоль;
- "Так, когда Будда достиг Просветления, он обнаружил, что больше не ощущает себя мишенью. Он не был больше ни телом, к которому рано или поздно ..."
- */
-let array = ['Он не был больше ни телом, к которому рано или поздно',
-  'он обнаружил,', 'Так, когда Будда достиг Просветления,',
-  '...', 'что больше не ощущает себя мишенью.'];
-
-let homeSentence = `${array[2]} ${array[1]} ${array[4]} ${array[0]} ${array[3]}`;
-console.log(homeSentence);
-
-/*
- 3. Добавьте 4 новых свойства в объект используя квадратные скобки и точку.
- */
-
-
-let myObj = {};
-
-myObj.companyName = 'Asterix';
-myObj.location = 'Kharkiv';
-myObj['wokers'] = 20;
-myObj['id'] = 6666;
-
-console.log(myObj);
-
-
-/*
- 4. Используя метод .slice, добавте недостающие символы в строку
- */
-
-let sliceString = 'Ytube';
-let l = sliceString[3];
-sliceString = sliceString.slice(0, 1) +'ou'+ sliceString[1].toLocaleUpperCase() + sliceString.slice(2);
-
-
-
-
-console.log(s); // YouTube 
-
-/*
- 5. Преобразуйте строку x, в максимально удобно читаемый вид
+1. Напишите функцию которая принимает в качестве аргумента массив
+и элемент массива и добавляет элемент в конец массива
 */
 
-let frameworks = [2.8, 'Angular', 'Angular2', 'React/Redux'];
-let x = `google released new version ${frameworks[1]} Math.floor(${frameworks[0]}) But real speed is ${frameworks[frameworks.length - 1]}`;
+let arr = [];
 
-console.log(x);
+let addToArray = (arrParam, newElem) => {
+  arrParam.push(newElem);
+};
+
+arr.push(35);
+arr.push(35)
+console.log(arr); // [35,35]
 
 
+
+addToArray(arr, { functionTypes: ['functionExpression', 'functionDeclaration'] });
+console.log(arr); // [35, 35, { functionTypes: ['functionExpression', 'functionDeclaration'] }]
+
+
+/*
+2. Напишите функцию которая получает 3 параметра 
+и возвращает объект типа: 
+    
+    {
+      argument1: param1,
+      argument2: param2,
+      argument3: param3,
+    }
+    
+*/
+
+let simpleObjectGenerator = (param1, param2, param3) => {
+
+
+  return {
+    argument1: 77,
+    argument2: [{ name: 'Egor' }, { name: 'Katya' }, { name: 'Vera' }],
+    argument3: 'PRIVET KAK DELA'
+  }
+};
+
+console.log(simpleObjectGenerator('protocol', { url: '22' }, 8000));
+
+/* {
+   argument1:'protocol',
+   argument2:{url:'22'},
+   argument3:8000
+}*/
+
+var userNames = [{ name: 'Egor' }, { name: 'Katya' }, { name: 'Vera' }];
+
+console.log(simpleObjectGenerator(77, userNames, 'privet kak dela chto novogo'.toUpperCase()));
+/* {
+ argument1:'77',
+ argument2:[{ name: 'Egor' }, { name: 'Katya' }, { name: 'Vera' }],
+ argument3:PRIVET KAK DELA CHTO NOVOGO
+ }*/
+
+
+var number2 = 150;
+var number3 = 3;
+let number4 = 10;
+
+let add = (a, b) => {
+  return a + b;
+};
+
+
+
+add(number4, number3);
+
+/*
+3.  Напишите функцию которая принимает 3 аргумента, 
+     третий аргумент - это объект.
+    
+    Функция создает объект где ключ это первый аргумент, 
+    а значение - второй аргумент
+    и добавляет свойство "name" из объекта 
+    и возвращает данный новый объект
+*/
+
+var myName = { name: 'Sergey' };
+
+let addNameToUser = (newKey, newValue, userName) => {
+
+  let newObject = {};
+  newObject[newKey] = newValue;
+  newObject.name = userName.name;
+  return newObject;
+
+};
+console.log(addNameToUser('family', '%Denisiuk%', myName));
+console.log(myName);
+
+
+
+/*
+-> @@ SUPER
+  Напишите функцию, которая будет возвращать 'Fizz' 
+   если передаваемый параметр кратен 3,
+   'Buzz', если передаваемый параметр кратен 5, 'FizzBuzz' - если параметер кратен 3 и 5
+   Если передаваемое число не кратно 3 или 5, то вернуть указанный параметр
+*/
+
+let fizzBuzz = num => {
+  let str = '';
+
+  if (!(num % 3)) {str += 'Fizz';}
+  if (!(num % 5)) {str += 'Buzz';}
+  if (!str.length) {return num;}
+  return str;
+};
+
+console.log(fizzBuzz(1)); // 1
+console.log(fizzBuzz(2)); // 2
+console.log(fizzBuzz(3)); // 'Fizz'
+console.log(fizzBuzz(5)); // 'Buzz'
+// ...
+console.log(fizzBuzz(15)); // 'FizzBuzz'
+console.log(fizzBuzz(21)); // 'Fizz'

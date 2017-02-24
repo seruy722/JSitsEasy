9130dcb9c262d518a355b909cc10c666e9005453
sergey <seruy722@gmail.com>
2017-02-25 01:50:50 +0200

    arrays2

--- js/main.js
+++ js/main.js
@@ -1,98 +1,164 @@
 ﻿'use strict';
 
 /*
- 1. Напишите функцию которая принимает 2 числа 
- и возвращает массив содержащий числа между первым числом и вторым числом;
+ 1. Переместите 0 в конец массива, остальные числа должны остаться неизменными
+ .сoncat();
+ example:
+ [1,false,2,0,3,null,0,4,0,25] => [1, false, 2, 3, null, 4, 25, 0, 0, 0]
+ [ 'a', 0, 0, 'b', null, 'c', 'd', 0, 1, false, 0, 1, 0, 3, [], 0, 1, 9, 0, 0, {}, 0, 0, 9 ] => ["a","b",null,"c","d",1,false,1,3,[],1,9,{},9,0,0,0,0,0,0,0,0,0,0]
+ [ 0, 1, null, 2, false, 1, 0 ] => [1,null,2,false,1,0,0]
  */
 
+let arr1 = [1, false, 2, 0, 3, null, 0, 4, 0, 25];
 
-let numbersBetween = (a, b) => {
-  let arr = [];
-  for (let i = a + 1; i < b; i++) {
-    arr.push(i);
+let moveZeroToEnd = arr => {
+  return arr.filter((element, index, arr) => {
+    return element !== 0;
+  }).concat(arr.filter((element, index, arr) => {
+    return element === 0;
+  }));
+};
+
+console.log(moveZeroToEnd(arr1));
+
+
+var array = [2, 1, 2000, 4, 5, 6, 7, 8, 9, 10];
+
+function myFilter() {
+  let args = [].slice.call(arguments);
+  return args.filter(value => {
+    return value % 2 === 0;
+  });
+}
+
+console.log(myFilter(2, 1, 2000, 4, 5, 6, 7, 8, 9, 10));
+
+
+/*
+ 2. Верните сумму двух найменьших чисел в массиве
+ 
+ [10,20,30,1,31,11,10] => 11
+ [-1,0,25] => -1
+ [-4,-10,25,10] => -14
+ [0,200,10,25,15] => 10
+ 
+ */
+
+let minimalNumber = arr => {
+  let sortedArr = arr.sort((leftEl, rightEl) => leftEl - rightEl);
+  return sortedArr[0] + sortedArr[1];
+};
+
+//minimalNumber([10, 20, 30, 1, 31, 11, 10]);
+console.log(minimalNumber([-4, -10, 25, 10]))
+
+
+/*
+ 3. Напишите функцию которая меняет местами имя и фамилию
+ nameShuffler('john McClane'); => "McClane john"
+ nameShuffler('Arnold Schwarzenegger'); => "Schwarzenegger Arnold"
+ nameShuffler('James Bond'); => "Bond James"
+ */
+/*3. Напишите функцию которая меняет местами имя и фамилию*/
+
+let nameShuffler = name => {
+  let arrFromNameSername = name.split(' ');
+  arrFromNameSername.reverse();
+  return arrFromNameSername.join(' ');
+};
+
+console.log(nameShuffler('john McClane'));// "McClane john"
+/*
+ 4. Напишите функцию которая принимает массив с именами и возвращает массив
+ в котором первая буква становится заглавной
+ capMe(['jo', 'nelson', 'jurie'])     // returns ['Jo', 'Nelson', 'Jurie']
+ capMe(['KARLY', 'DANIEL', 'KELSEY']) // returns ['Karly', 'Daniel', 'Kelsey']
+ */
+let arrNames = ['KARLY', 'DANIEL', 'KELSEY'];
+
+let capMe = arr => {
+  for (let i = 0; i < arr.length; i++) {
+    let name = arr[i];
+    arr[i] = name.slice(0, 1).toUpperCase() + name.slice(1).toLowerCase();
   }
   return arr;
 };
 
+console.log(capMe(arrNames));
 
-console.log(numbersBetween(1, 5)); // 2,3,4
-console.log(numbersBetween(3, 6)); // 4,5
-console.log(numbersBetween(12, 15)); // 13,14
 
+
+
+//@SUPER
 /*
- 2. Перепешите задачу FizzBuzz, но с использованием цикла. 
- Расчет чисел должен идти до 100
-
+ 1. Найдите Число отсутствующее в заданной последовательности
+ 
+ example:
+ [1,3,5,9] => 7
+ [0,8,16,32] => 24
+ [0,16,24,32] => 8
+  
  */
+let random = arr => {
+  let step = (arr[arr.length - 1] - arr[0]) / arr.length;
 
-let fizzBuzz = num => {
-  let str = '';
-  if (!(num % 3)) {
-    str = str + 'Fizz';
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] + step !== arr[i + 1]) {
+      return arr[i] + step;
+    }
   }
-  if (num % 5 == 0) {
-    str = str + 'Buzz';
-  }
-  if (!str.length) {
-    return num;
-  }
-  return str;
+  console.log(arr[1]);
+  //return arr[1] - arr[0];
+
 };
 
-let fizzBuzz100 = () => {
-  let num = 1;
-  while (num < 100) {
-    console.log(fizzBuzz(num));
-    num++;
-  }
+console.log(random([1, 3, 5, 9]));
+random([0, 8, 16, 32]);
+random([0, 16, 24, 32]);
+random([4, 6, 8, 10]);
+
+// Напишите функцию которая ищет в массиве hello
+let test = [[], { html: 'css' }, 'hello', { hello: 'world' }];
+let test2 = [[], { html: 'css' }, 'hello'];
+let test3 = [20, 30, 50, 60];
+
+
+let isHelloInArray = arr => {
+  return arr.some(function(element, index, arr) {
+    return element.name;
+  });
+
 };
 
-fizzBuzz100();
+console.log(isHelloInArray(test)) //true
+console.log(isHelloInArray(test2)) //false
 
 
-/*
- 3. Напишите функцию которая принимает 1 аргумент - массив 
- И возвращает новый массив содержащий типы значений переменных
- */
 
-let arr = [ 1, null, undefined, 'str', {}, [], function() {} ];
-// let arr = ['privet', 12, {}, [1, 2]];
-let arr2 = ['privet', 12, {}, [1, 2]];
-
-let returnArr = argument => {
-  let newArr = [];
-  for (let i = 0; i < argument.length; i++) {
-
-    newArr.push(typeof (argument[i]));
-  }
-  return newArr;
-};
-console.log(returnArr(arr));
-console.log(returnArr(arr2));
 
 
 /*
- @@SUPER@@. Вам дан массив array, содержащий внутри объекты. 
- Напишите функцию которая внутри цикла проходится по каждому элементу массива 
- И проверяет какой тип данных содержит свойство age, если тип данных NaN, тогда добавляет данному объекту свойство unknownAge: true
- Далее создайте новый массив содержащий все объекты есть свойство unknownAge:true
+ 2. Напишите функция которая преобразовывает открывает скобки всех вложенных внутри массивов
+ Необходимо реализовать рекурсивный фызов функции.
+ Функция должна открывать любое количество внутренних массивов
+ 
+ example:
+ [[1,2],[3,[4]],5, 10] => [1,2,3,4,5,10]
+ [25,10,[10,[15]]] => [25,10,10,15]
+ 
  */
+let arr2 = [[1, 2], [3, [4]], 5, 10];
 
-let array = Array.from({ length: 35 }).map((value, index) => index % 2 ? { age: index + 2 } : { age: NaN });
-console.log(array); //[ {age:NaN}, {age:3}, {age:NaN}, {age:5}, {age:NaN}, {age:7} ....]
-console.log(array.length); // 35
-
-
-let solution = arr => {
+let openBraces = arr => {
   let newArr = [];
   for (let i = 0; i < arr.length; i++) {
-    let objectInArray = arr[i];
-    if (isNaN(objectInArray.age)) {
-      objectInArray.unknownAge = true;
-      newArr.push(objectInArray);
+    if (Array.isArray(arr[i])) {
+      newArr = newArr.concat(openBraces(arr[i]));
+    } else {
+      newArr.push(arr[i]);
     }
   }
   return newArr;
 };
 
-console.log(solution(array));
+console.log(openBraces(arr2));

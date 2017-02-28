5c2c66eccc9f8eee4be5de60634665c02821e7bb
sergey <seruy722@gmail.com>
2017-02-28 23:19:54 +0200

    class throw proto

--- inheritance/js/inheritance.js
+++ inheritance/js/inheritance.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// Normal proto
 function Animal(name) {
   this.speed = 0;
   this.name = name;
@@ -20,4 +21,27 @@ var animal = new Animal('Зверь');
 alert( animal.speed ); // 0, начальная скорость
 animal.run(3); // Зверь бежит, скорость 3
 animal.run(10); // Зверь бежит, скорость 13
+animal.stop(); // Зверь стоит
+
+// Class throw proto
+function Animal(name) {
+  this.name = name;
+  this.speed = 0;
+}
+
+Animal.prototype.run = function(speed) {
+  this.speed += speed;
+  alert( this.name + ' бежит, скорость ' + this.speed );
+};
+
+Animal.prototype.stop = function() {
+  this.speed = 0;
+  alert( this.name + ' стоит' );
+};
+
+var animal = new Animal('Зверь');
+
+alert( animal.speed ); // 0, свойство взято из прототипа
+animal.run(5); // Зверь бежит, скорость 5
+animal.run(5); // Зверь бежит, скорость 10
 animal.stop(); // Зверь стоит

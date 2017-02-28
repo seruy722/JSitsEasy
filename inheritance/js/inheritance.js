91151311691b5b54b081884a09ab9416ab228df4
sergey <seruy722@gmail.com>
2017-02-28 23:15:09 +0200

    func-constructor

--- inheritance/js/inheritance.js
+++ inheritance/js/inheritance.js
@@ -1,2 +1,23 @@
 'use strict';
 
+function Animal(name) {
+  this.speed = 0;
+  this.name = name;
+
+  this.run = function(speed) {
+    this.speed += speed;
+    alert( this.name + ' бежит, скорость ' + this.speed );
+  };
+
+  this.stop = function() {
+    this.speed = 0;
+    alert( this.name + ' стоит' );
+  };
+};
+
+var animal = new Animal('Зверь');
+
+alert( animal.speed ); // 0, начальная скорость
+animal.run(3); // Зверь бежит, скорость 3
+animal.run(10); // Зверь бежит, скорость 13
+animal.stop(); // Зверь стоит
